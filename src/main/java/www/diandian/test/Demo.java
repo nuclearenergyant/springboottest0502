20c7d8768b64afca9bc004ff45110f030e185379
nuclearenergyant <1483773408@qq.com>
2020-05-26 09:46:23 +0800

    test

---
+++ src/main/java/www/diandian/test/Demo.java
@@ -0,0 +1,16 @@
+package www.diandian.test;
+
+/**
+ * \* Created with IntelliJ IDEA.
+ * \* User: weige
+ * \* Date: 2020/5/26
+ * \* Time: 9:45
+ * \* Description: www.diandian.在线
+ * \* 功能说明：
+ * \
+ */
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("I Love You");
+    }
+}
